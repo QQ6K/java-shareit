c60acabbc3a8a398be9cb89bdf3e43f2fb8f7798
QQ6K <golyshev491@gmail.co>
2022-09-28 21:26:25 +0300

    style: ItemsService

--- src/main/java/ru/practicum/shareit/item/interfaces/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/interfaces/ItemService.java
@@ -3,13 +3,18 @@ package ru.practicum.shareit.item.interfaces;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 public interface ItemService {
 
     Optional<Item> updateItem(Long userId, Long itemId, ItemDto itemDto);
+
     void deleteItem(Long itemId);
+
     Item readById(Long itemId);
+
     Collection<Item> readAll(Long userId);
 
     Optional<Item> createItem(Long userId, ItemDto itemDto);
