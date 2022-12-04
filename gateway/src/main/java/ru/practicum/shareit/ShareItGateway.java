d216b709f8cf5d165e119c0c4fb95b9fd61f02ed
QQ6K <golyshev491@gmail.co>
2022-12-05 01:01:10 +0300

    style: checkstyle

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItGateway {
 	public static void main(String[] args) {
-		System.getProperties().put("server.port",8080 );
+		System.getProperties().put("server.port",8080);
 		System.getProperties().put("shareit-server.url","http://localhost:9090");
 		SpringApplication.run(ShareItGateway.class, args);
 	}
